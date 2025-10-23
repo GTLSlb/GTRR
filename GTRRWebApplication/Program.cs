605c6848311bde8f722dec442d7154b2004dfe03
RimAlHaddad <rimalh@Gtls.com.au>
2025-10-23 14:44:59 +0300

    fing logs and add staging cors policy domain

--- GTRRWebApplication/Program.cs
+++ GTRRWebApplication/Program.cs
@@ -1,14 +1,32 @@
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
+
+
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Information()
+    .Enrich.FromLogContext()
+    .WriteTo.File(
+        path: "Logs/app-.log",
+        rollingInterval: RollingInterval.Day,
+        retainedFileCountLimit: 14,
+        shared: true,
+        outputTemplate:
+        "[{Timestamp:yyyy-MM-dd HH:mm:ss.fff}] [{Level:u3}] ({ProcessId}/{ThreadId}) {SourceContext} | {Message:lj}{NewLine}{Exception}")
+    .CreateLogger();
+
 
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowSpecificOrigins", policy =>
     {
         policy.WithOrigins(
-           
-            "https://gtrr.gtls.store",
+
+            "https://gtrr.gtls.com.lb/e",
+            "https://gtrr-api.gtls.com.lb/",
             "http://localhost:3000",
-            "https://gtrr-api.gtls.store"
+            "http://localhost:3001"
+          
         )
         .AllowAnyHeader()
         .AllowAnyMethod();
@@ -19,7 +37,7 @@ builder.Services.AddCors(options =>
 builder.Services.AddControllers();
 
 builder.Services.AddOpenApi();
-
+builder.Host.UseSerilog();
 var app = builder.Build();
 
 
@@ -35,5 +53,6 @@ app.UseCors("AllowSpecificOrigins");
 app.UseAuthorization();
 
 app.MapControllers();
+
 
 app.Run();
