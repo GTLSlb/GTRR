cabb70f00440c2341bb88105c59c0bcf09fe8cd3
RimAlHaddad <rimalh@Gtls.com.au>
2025-10-23 13:04:19 +0300

    logs

--- GTRRWebApplication/Program.cs
+++ GTRRWebApplication/Program.cs
@@ -1,4 +1,17 @@
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
+
+
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Debug()
+    .WriteTo.Console() // optional, logs to console
+    .WriteTo.File(
+        path: "Logs/log.txt",
+        rollingInterval: RollingInterval.Day, // creates a new file each day
+        outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff} [{Level:u3}] {Message:lj}{NewLine}{Exception}"
+    )
+    .CreateLogger();
 
 builder.Services.AddCors(options =>
 {
@@ -19,7 +32,7 @@ builder.Services.AddCors(options =>
 builder.Services.AddControllers();
 
 builder.Services.AddOpenApi();
-
+builder.Host.UseSerilog();
 var app = builder.Build();
 
 
